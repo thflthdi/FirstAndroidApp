d25b8c4c37d51bffef26b3ab55d7b9f92ac70a3b
thflthdi <thflthdi@gmail.com>
2022-05-25 20:45:40 +0900

    제트팩 실습 : 툴바, 메뉴, 프래그먼트, 리사이클러 뷰, 뷰 페이저2, 드로어 레이아웃

--- AndroidLab/ch11_jetpack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
+++ AndroidLab/ch11_jetpack/src/main/java/com/example/ch11_jetpack/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.ch11_jetpack
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.ch11_jetpack.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
+    //뷰 페이지 어댑터
+    class MyFragmentPagerAdapter(activity: FragmentActivity) :
+        FragmentStateAdapter(activity) {
+        val fragments: List<Fragment>
+
+        init {
+            fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+        }
+
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawer,
+            R.string.drawer_opened,
+            R.string.drawer_closed
+        )
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+        val adapter = MyFragmentPagerAdapter(this)
+        binding.viewpager.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        val menuItem = menu.findItem(R.id.menu_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                Log.d("query", "search text: $p0")
+                return true
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
