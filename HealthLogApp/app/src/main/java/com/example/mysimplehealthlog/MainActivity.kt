38bb2a125350416f3dbf89a19821a190addda58f
thflthdi <thflthdi@gmail.com>
2022-07-15 15:57:05 +0900

    각 페이지에 맞는 framgent 생성, viewPager2로 페이지 이동 구현

--- HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/MainActivity.kt
+++ HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/MainActivity.kt
@@ -6,18 +6,31 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.mysimplehealthlog.databinding.ActivityMainBinding
 import com.example.mysimplehealthlog.databinding.CustomTabItemBinding
 import com.google.android.material.tabs.TabLayout
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mContext: Context
+
+    class FragmentPagerAdapter(activity: FragmentActivity) :
+        FragmentStateAdapter(activity) {
+        val fragments: List<Fragment>
+        init {
+            fragments = listOf(HomeFragment(), AnalysisFragment(), PlanFragment(), PlusFragment())
+        }
+
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        mContext = applicationContext
 
         val tabLayout = binding.tabs
         val tab_home: TabLayout.Tab = binding.tabs.newTab()
@@ -33,22 +46,31 @@ class MainActivity : AppCompatActivity() {
         tab_plus.setCustomView(matchTabIcon("더보기"))
         tabLayout.addTab(tab_plus)
 
-        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
+        val adapter = FragmentPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+
+
+//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
 //                val transaction = supportFragmentManager.beginTransaction()
 //                when (tab?.text) {
 //                    "홈" -> transaction.replace(R.id.tab_content, HomeFragment())
+//                    "통계" -> transaction.replace(R.id.tab_content, HomeFragment())
+//                    "플랜" -> transaction.replace(R.id.tab_content, HomeFragment())
+//                    "더보기" -> transaction.replace(R.id.tab_content, HomeFragment())
 //                }
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
-            }
-        })
+//                transaction.commit()
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                //tab의 상태가 선택 상태 -> 선택 되지 않음으로 변경
+//
+//            }
+//        })
     }
 
     private fun matchTabIcon(tabName: String): View {
@@ -57,7 +79,6 @@ class MainActivity : AppCompatActivity() {
         when (tabName) {
             "홈" -> {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_home)
-                Log.d("test", "1 ${tabView.tabLayout}, ${tabView.tabText.text}, ${tabView.tabIc}")
                 return tabView.tabLayout
             }
             "통계" -> {
