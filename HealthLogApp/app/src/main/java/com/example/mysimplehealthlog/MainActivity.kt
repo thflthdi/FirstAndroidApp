4777eac8bc563833b149b1c4b7c1236238f8f714
thflthdi <thflthdi@gmail.com>
2022-07-18 17:19:44 +0900

    하단 탭 select 색상 변경

--- HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/MainActivity.kt
+++ HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mysimplehealthlog
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.mysimplehealthlog.databinding.ActivityMainBinding
 import com.example.mysimplehealthlog.databinding.CustomTabItemBinding
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,70 +36,60 @@ class MainActivity : AppCompatActivity() {
 
         val tabLayout = binding.tabs
         val tab_home: TabLayout.Tab = binding.tabs.newTab()
-        tab_home.setCustomView(matchTabIcon("홈"))
+//        tab_home.setCustomView(matchTabIcon("홈"))
         tabLayout.addTab(tab_home)
         val tab_anal: TabLayout.Tab = binding.tabs.newTab()
-        tab_anal.setCustomView(matchTabIcon("통계"))
+//        tab_anal.setCustomView(matchTabIcon("통계"))
         tabLayout.addTab(tab_anal)
         val tab_plan: TabLayout.Tab = binding.tabs.newTab()
-        tab_plan.setCustomView(matchTabIcon("플랜"))
+//        tab_plan.setCustomView(matchTabIcon("플랜"))
         tabLayout.addTab(tab_plan)
         val tab_plus: TabLayout.Tab = binding.tabs.newTab()
-        tab_plus.setCustomView(matchTabIcon("더보기"))
+//        tab_plus.setCustomView(matchTabIcon("더보기"))
         tabLayout.addTab(tab_plus)
 
         val adapter = FragmentPagerAdapter(this)
         binding.viewPager.adapter = adapter
+        TabLayoutMediator(binding.tabs, binding.viewPager) {tab, position ->
+            when(position) {
+                0 -> tab.setCustomView(matchTabIcon("홈"))
+                1 -> {
+                    tab.setCustomView(matchTabIcon("통계"))
+                    Log.d("test", "when")
+                }
+                2 -> tab.setCustomView(matchTabIcon("플랜"))
+                3 -> tab.setCustomView(matchTabIcon("더보기"))
+            }
+        }.attach()
 
-
-//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-//            override fun onTabSelected(tab: TabLayout.Tab?) {
-//                val transaction = supportFragmentManager.beginTransaction()
-//                when (tab?.text) {
-//                    "홈" -> transaction.replace(R.id.tab_content, HomeFragment())
-//                    "통계" -> transaction.replace(R.id.tab_content, HomeFragment())
-//                    "플랜" -> transaction.replace(R.id.tab_content, HomeFragment())
-//                    "더보기" -> transaction.replace(R.id.tab_content, HomeFragment())
-//                }
-//                transaction.commit()
-//            }
-//
-//            override fun onTabReselected(tab: TabLayout.Tab?) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onTabUnselected(tab: TabLayout.Tab?) {
-//                //tab의 상태가 선택 상태 -> 선택 되지 않음으로 변경
-//
-//            }
-//        })
     }
 
-    private fun matchTabIcon(tabName: String): View {
+    private fun matchTabIcon(tabName: String, isSelected: Boolean = false): View {
         val tabView = CustomTabItemBinding.inflate(layoutInflater)
         tabView.tabText.text = tabName
         when (tabName) {
             "홈" -> {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_home)
-                return tabView.tabLayout
             }
             "통계" -> {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_anal)
-                return tabView.tabLayout
             }
             "플랜" -> {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_plan)
-                return tabView.tabLayout
             }
             "더보기" -> {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_plus)
-                return tabView.tabLayout
             }
             else -> {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_home)
-                return tabView.tabLayout
             }
         }
+        if (isSelected){
+            Log.d("test", "${isSelected}")
+            tabView.tabIc.setImageResource(R.drawable.ic_tab_home)
+            tabView.tabText.setTextColor(Color.BLACK)
+        }
+        return tabView.tabLayout
 
     }
 
