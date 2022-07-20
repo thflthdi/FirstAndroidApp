f720087d596a99351bba34fc6d2956e16bf9be65
thflthdi <thflthdi@gmail.com>
2022-07-20 11:04:44 +0900

    viewpager 스와이프 막기

--- HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/MainActivity.kt
+++ HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.mysimplehealthlog.databinding.ActivityMainBinding
 import com.example.mysimplehealthlog.databinding.CustomTabItemBinding
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     class FragmentPagerAdapter(activity: FragmentActivity) :
         FragmentStateAdapter(activity) {
         val fragments: List<Fragment>
+
         init {
             fragments = listOf(HomeFragment(), AnalysisFragment(), PlanFragment(), PlusFragment())
         }
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         override fun createFragment(position: Int): Fragment = fragments[position]
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,22 +39,23 @@ class MainActivity : AppCompatActivity() {
 
         val tabLayout = binding.tabs
         val tab_home: TabLayout.Tab = binding.tabs.newTab()
-//        tab_home.setCustomView(matchTabIcon("홈"))
+        tab_home.setCustomView(matchTabIcon("홈"))
         tabLayout.addTab(tab_home)
         val tab_anal: TabLayout.Tab = binding.tabs.newTab()
-//        tab_anal.setCustomView(matchTabIcon("통계"))
+        tab_anal.setCustomView(matchTabIcon("통계"))
         tabLayout.addTab(tab_anal)
         val tab_plan: TabLayout.Tab = binding.tabs.newTab()
-//        tab_plan.setCustomView(matchTabIcon("플랜"))
+        tab_plan.setCustomView(matchTabIcon("플랜"))
         tabLayout.addTab(tab_plan)
         val tab_plus: TabLayout.Tab = binding.tabs.newTab()
-//        tab_plus.setCustomView(matchTabIcon("더보기"))
+        tab_plus.setCustomView(matchTabIcon("더보기"))
         tabLayout.addTab(tab_plus)
 
         val adapter = FragmentPagerAdapter(this)
         binding.viewPager.adapter = adapter
-        TabLayoutMediator(binding.tabs, binding.viewPager) {tab, position ->
-            when(position) {
+        binding.viewPager.isUserInputEnabled = false
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            when (position) {
                 0 -> tab.setCustomView(matchTabIcon("홈"))
                 1 -> {
                     tab.setCustomView(matchTabIcon("통계"))
@@ -84,7 +88,7 @@ class MainActivity : AppCompatActivity() {
                 tabView.tabIc.setImageResource(R.drawable.ic_tab_home)
             }
         }
-        if (isSelected){
+        if (isSelected) {
             Log.d("test", "${isSelected}")
             tabView.tabIc.setImageResource(R.drawable.ic_tab_home)
             tabView.tabText.setTextColor(Color.BLACK)
