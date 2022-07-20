8a229d37fc43256673a9ddd6f7cdf00d83115726
thflthdi <thflthdi@gmail.com>
2022-07-20 15:31:18 +0900

    TODO 추가

--- HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/HomeFragment.kt
+++ HealthLogApp/app/src/main/java/com/example/mysimplehealthlog/HomeFragment.kt
@@ -12,6 +12,7 @@ import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.spans.DotSpan
 
+// calendar setting
 class CalendarSetting(actionDay: Collection<CalendarDay>) : DayViewDecorator {
     var actionDay: HashSet<CalendarDay> = HashSet(actionDay)
 
@@ -32,17 +33,20 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(layoutInflater)
         val calendar = binding.calendarView
 
-
+        // activity 호출, 운동 페이지로 이동
         binding.exerciseButton.setOnClickListener {
             (activity as MainActivity).replaceFragment(ExerciseFragment())
         }
 
+        // calendar
+        // TODO("커스텀 원 사이즈 줄이기, dot 말고 원으로 변경")
         calendar.setSelectedDate(CalendarDay.today())
         val dayList = mutableListOf<CalendarDay>()
         for (active in 1..15) {
             dayList.add(CalendarDay.from(2022, 7, active))
         }
         calendar.addDecorator(CalendarSetting(dayList))
+
         return binding.root
     }
 }
